14eab57f57f227d42b88d86291a49751ffe18925
Ryan Kruse <ryan@kruseonline.net>
2025-07-09 16:47:56 -0500

    more test prep

---
+++ tests/grant.test.ts
@@ -0,0 +1,23 @@
+import prettier from "prettier";
+import { describe, test, expect } from "vitest";
+import * as sqlPlugin from "../src";
+
+// Register the plugin with Prettier
+const options = {
+    plugins: [sqlPlugin],
+    tabWidth: 4,
+    parser: "sql",
+};
+
+describe("grant", () => {
+    test.skip("Simple grant", async () => {
+        const unformatted = `grant usage ON schema instat to role READERS; grant SELECT ON FUTURE TABLES IN schema instat to role READERS; grant SELECT ON FUTURE VIEWS IN schema instat to role READERS;`;
+
+        const expected = `GRANT USAGE ON SCHEMA instat TO ROLE READERS;
+GRANT SELECT ON FUTURE TABLES IN SCHEMA instat TO ROLE READERS;
+GRANT SELECT ON FUTURE VIEWS IN SCHEMA instat TO ROLE READERS;`;
+
+        const formatted = await prettier.format(unformatted, options);
+        expect(formatted.trim()).toBe(expected);
+    });
+});
---
+++ tests/update.test.ts
@@ -0,0 +1,45 @@
+import prettier from "prettier";
+import { describe, test, expect } from "vitest";
+import * as sqlPlugin from "../src";
+
+// Register the plugin with Prettier
+const options = {
+    plugins: [sqlPlugin],
+    tabWidth: 4,
+    parser: "sql",
+};
+
+describe("UPDATE", () => {
+    test.skip("Simple update", async () => {
+        const unformatted = `
+      update public.player set birthday = current_date, last_updated = current_timestamp where id > 1000;
+    `;
+
+        const expected = `UPDATE public.player
+   SET birthday = current_date
+     , last_updated = current_timestamp
+WHERE id > 1000
+;`;
+
+        const formatted = await prettier.format(unformatted, options);
+        expect(formatted.trim()).toBe(expected);
+    });
+
+    test.skip("Update from subquery", async () => {
+        const unformatted = `
+      update public.player set birthday = current_date, last_updated = current_timestamp where id in (select id from public.player where id > 1000);
+    `;
+
+        const expected = `UPDATE public.player
+   SET birthday = CURRENT_DATE
+     , last_updated = CURRENT_TIMESTAMP
+WHERE id IN (
+    SELECT id
+    FROM public.player
+    WHERE id > 1000
+)
+;`;
+        const formatted = await prettier.format(unformatted, options);
+        expect(formatted.trim()).toBe(expected);
+    });
+});
